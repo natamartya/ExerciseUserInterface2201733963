9e4dc269a16eb56ec64f0c190dfc1300013849ea
Tashya <natamartya@gmail.com>
2020-10-28 12:15:13 +0700

    Exercise User Interface

---
+++ app/src/main/java/com/example/userinterfacelayout_2201733963/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.userinterfacelayout_2201733963;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
